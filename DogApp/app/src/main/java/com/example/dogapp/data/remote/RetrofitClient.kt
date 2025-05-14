1b68739b1192a7d558c83f9e0f8d840cbf66b7f9
Brayan Steven Narvaez Valdes <brayanss2018@gmail.com>
2025-05-13 20:42:50 -0500

    feat: Integrate Retrofit for API calls and update UI components for breed management

---
+++ DogApp/app/src/main/java/com/example/dogapp/data/remote/RetrofitClient.kt
@@ -0,0 +1,28 @@
+package com.example.dogapp.data.remote
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://dog.ceo/api/"
+
+    private val interceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val dogCeoApiService: DogCeoApiService by lazy {
+        retrofit.create(DogCeoApiService::class.java)
+    }
+}
